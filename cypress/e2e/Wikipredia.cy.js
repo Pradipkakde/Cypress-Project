2b556eadeb1cd732a16e82a8c6b2978c8402e958
Pradipkakde <kakde.pradip1@gmail.com>
2024-02-22 12:00:35 +0530

    Automation test done with wikipedia

--- cypress/e2e/Wikipredia.cy.js
+++ cypress/e2e/Wikipredia.cy.js
@@ -1,5 +1,10 @@
 describe('dynamic test of the wikipedia',()=>{
-    it('test url',()=>{
+    it('Dynamic list',()=>{
+        //step first visit website
     cy.visit("https://www.wikipedia.org/")
+    //step second type in serarch box for getting listign suggetions
+    cy.get("#search-input").click().type('mcd')
+    //step three select macdownld from auto suggetsion
+    cy.get(".suggestion-link").contains("onald's").click()
     })
 })
