5e3fa38a544cecd7cd653909ee607d77fb6e05e4
Pradipkakde <kakde.pradip1@gmail.com>
2024-02-22 20:45:08 +0530

    test alerts

--- cypress/e2e/Alerts.cy.js
+++ cypress/e2e/Alerts.cy.js
@@ -1,6 +1,15 @@
 describe('alerts',()=>{
-    it('test alearts',()=>{
-        cy.visit("")
+    it('simple alerts',()=>{
+        //step first visit website
+        cy.visit("https://the-internet.herokuapp.com/javascript_alerts")
+        //step second click on button for aleart
+        cy.get("button[onclick='jsAlert()']").click()
+        //step 3 get the text from alert
+        cy.on('window:confirm',(t)=>{
+            //I am a js Alert
+           expect(t).to.contains("i am a js aleart")
 
+     })//assert we are checking/ validate
+     cy.get("#result").should('have.txt',"You successfully click")
     })
 })
